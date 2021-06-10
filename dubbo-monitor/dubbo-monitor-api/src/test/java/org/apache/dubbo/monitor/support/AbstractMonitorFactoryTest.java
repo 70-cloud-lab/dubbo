6c00e4a15bb9f5fe60fab00d4e63d7cbf5785769
张远征hd <zhangyzhd@163.com>
2021-06-10 14:55:07 +0800

    Update AbstractMonitorFactoryTest.java

--- dubbo-monitor/dubbo-monitor-api/src/test/java/org/apache/dubbo/monitor/support/AbstractMonitorFactoryTest.java
+++ dubbo-monitor/dubbo-monitor-api/src/test/java/org/apache/dubbo/monitor/support/AbstractMonitorFactoryTest.java
@@ -65,8 +65,10 @@ public class AbstractMonitorFactoryTest {
         URL url = URL.valueOf("dubbo://" + NetUtils.getLocalAddress().getHostAddress() + ":2233");
         Monitor monitor1 = monitorFactory.getMonitor(url);
         Monitor monitor2 = monitorFactory.getMonitor(url);
-        if (monitor1 == null || monitor2 == null) {
-            Thread.sleep(500);
+        int i = 0;
+        while ((monitor1 == null || monitor2 == null) && i < 200) {
+            i++;
+            Thread.sleep(10);
             monitor1 = monitorFactory.getMonitor(url);
             monitor2 = monitorFactory.getMonitor(url);
         }
@@ -78,8 +80,10 @@ public class AbstractMonitorFactoryTest {
         URL url = URL.valueOf("dubbo://" + NetUtils.getLocalAddress().getHostName() + ":2233");
         Monitor monitor1 = monitorFactory.getMonitor(url);
         Monitor monitor2 = monitorFactory.getMonitor(url);
-        if (monitor1 == null || monitor2 == null) {
-            Thread.sleep(500);
+        int i = 0;
+        while ((monitor1 == null || monitor2 == null) && i < 200) {
+            i++;
+            Thread.sleep(10);
             monitor1 = monitorFactory.getMonitor(url);
             monitor2 = monitorFactory.getMonitor(url);
         }
@@ -92,8 +96,10 @@ public class AbstractMonitorFactoryTest {
         URL url2 = URL.valueOf("dubbo://" + NetUtils.getLocalHost() + ":2233?group=bbb");
         Monitor monitor1 = monitorFactory.getMonitor(url1);
         Monitor monitor2 = monitorFactory.getMonitor(url2);
-        if (monitor1 == null || monitor2 == null) {
-            Thread.sleep(500);
+        int i = 0;
+        while ((monitor1 == null || monitor2 == null) && i < 200) {
+            i++;
+            Thread.sleep(10);
             monitor1 = monitorFactory.getMonitor(url1);
             monitor2 = monitorFactory.getMonitor(url2);
         }
